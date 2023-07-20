51b5adaa2a815807897557713cca03814706e7b0
notcrimson <artden1mc@gmail.com>
2023-07-20 20:56:48 +0300

    Finished navbar And Initialize Header and Brand

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -41,11 +41,19 @@ const Navbar = () => {
         <button type="button">Sign up</button>
       </div>
       <div className="gpt3__navbar-menu">
-        {toggleMenu ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} /> : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu ? (
+          <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+        ) : (
+          <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+        )}
         {toggleMenu && (
           <div className="gpt3__navbar-menu_container scale-up-center">
-            <div className="gpt3__navbar-meu_container-links">
+            <div className="gpt3__navbar-menu_container-links">
               <Menu />
+              <div className="gpt3__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
             </div>
           </div>
         )}
